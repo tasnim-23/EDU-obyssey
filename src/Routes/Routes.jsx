e2f76c04625238daa2da99e673b01d7f785e46d4
Erina <erinatasnim007@gmail.com>
2024-12-02 01:49:01 +0600

    Created a route file containing home page link with the help of react router

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+
+ const router = createBrowserRouter(
+    [
+        {
+            path:"/",
+            element:<Root></Root>,
+            children:[]
+        }
+    ]
+ )
+
+// const Routes = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     );
+// };
+
+// export default Routes;
